f4de023f952cb22c201f30a026cc466f95ab95ed
spencerltan <33206008+spencerltan@users.noreply.github.com>
2026-04-01 11:13:54 -0500

    v2 noshadowstep

--- NoShadowstep/NoShadowstep.lua
+++ NoShadowstep/NoShadowstep.lua
@@ -3,6 +3,10 @@ f:SetSize(400, 100)
 f:SetPoint("CENTER", -200, 80) -- 200px to the left
 f:SetFrameStrata("TOOLTIP")
 
+-- Audio setup
+local soundFolder = [[Interface\AddOns\NoShadowstep\]]
+local playedNoShadowstepSound = false
+
 -- Function to create the "NO [ICON]" group
 local function CreateMissingGroup(parent, color)
     local group = CreateFrame("Frame", nil, parent, "BackdropTemplate")
@@ -55,6 +59,13 @@ f:SetScript("OnUpdate", function(self, elapsed)
     -- Only show when out of charges
     if charges <= 0 then
         redAlert:Show()
+        
+        -- TTS Logic: Play sound once when charges hit 0
+        if not playedNoShadowstepSound then
+            playedNoShadowstepSound = true
+            PlaySoundFile(soundFolder .. "noshadowstep.ogg", "Master")
+        end
+
         if #rechargeEndTimes > 0 then
             local firstRecharge = rechargeEndTimes[1]
             redAlert.timer:SetText(string.format("- %.1f", math.max(0, firstRecharge - now)))
@@ -63,6 +74,7 @@ f:SetScript("OnUpdate", function(self, elapsed)
         end
     else
         redAlert:Hide()
+        playedNoShadowstepSound = false -- Reset flag so it can play again next time
     end
 end)
 
@@ -76,11 +88,12 @@ f:SetScript("OnEvent", function(self, event, unit, _, spellID)
 
         C_Timer.After(thisRechargeEnd - now, function()
             charges = math.min(MAX_CHARGES, charges + 1)
-            table.remove(rechargeEndTimes, 1)
+            if #rechargeEndTimes > 0 then table.remove(rechargeEndTimes, 1) end
         end)
     elseif event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_REGEN_ENABLED" then
         charges = MAX_CHARGES
         rechargeEndTimes = {}
+        playedNoShadowstepSound = false
     end
 end)
 
